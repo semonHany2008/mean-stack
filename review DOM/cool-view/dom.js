1f27230dbf14145a25b405f268142de2e7555394
semonHany2008 <semonhany848@gmail.com>
2025-07-03 19:50:18 +0300

    Add files via upload

---
+++ review DOM/cool-view/dom.js
@@ -0,0 +1,8 @@
+let header = document.getElementById("header");
+header.style.textAlign = "right";
+
+let nav = document.getElementById("nav");
+nav.style.listStyleType = "circle";
+nav.style.margin = "0";
+nav.style.width = "fit-content";
+nav.style.margin = "5px auto";
