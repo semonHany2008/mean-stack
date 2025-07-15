02f0ee1464a38bf38467724548965577eea3f5c2
semonHany2008 <semonhany848@gmail.com>
2025-07-15 04:15:15 +0300

    Delete DB directory

--- DB/dbConnection.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-export const dbConnection = mongoose
-  .connect("mongodb://localhost:27017/NTI")
-  .then(() => {
-    console.log("mongoDB connected!");
-  })
-  .catch((error) => console.error(error)); //NTI here is a database, and this string represent URI(universal resource identifier)
